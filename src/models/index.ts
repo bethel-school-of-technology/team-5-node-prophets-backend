ce24c5a7098caac6b90b96998767f2263b22731a
ionic-project <carlped417@gmail.com>
2023-10-07 11:49:35 -0500

    hk

--- src/models/index.ts
+++ src/models/index.ts
@@ -9,7 +9,7 @@ const password = "Password1!";
 const sequelize = new Sequelize(dbName, username, password, {
   host: "127.0.0.1",
   port: 3306,
-  dialect: "mysql",
+  dialect: "mysql"
 });
 
 UserFactory(sequelize);
